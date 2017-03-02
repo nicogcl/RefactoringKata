7831d44055f765b64af4df55b4cbe168f635181a
nicogcl <nicogcl@gmail.com>
2017-03-02 19:23:17 +0100

    Test driven refactoring

--- WalletKata.Test/WalletServiceTest.cs
+++ WalletKata.Test/WalletServiceTest.cs
@@ -1,13 +1,41 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using WalletKata.Wallets;
 
 namespace WalletKata.Test
 {
     public class WalletServiceTest
     {
         [Test]
-        public void TestSomething()
+        public void TestGetWalletByUser1()
         {
-            
+            // First goal is to be able to compile/run this kind of code
+
+            // Simplest case
+            var wallet = new WalletService();
+            var user = new Users.User();
+
+            Users.User loggedUser = null;
+            // TODO Mock UserSession 
+
+            user.AddFriend(loggedUser);
+
+            // Link wallets to user
+            // TODO
+            List<Wallet> userWallets = null;
+
+            // TODO Mock WalletDAO
+            var wallets = wallet.GetWalletsByUser(user);
+
+            // Does NUnit Assert work on container(object)..?
+            Assert.IsNotNull(wallets);
+            Assert.AreEqual(userWallets.Count, wallets.Count);
+            for (int i = 0; i < userWallets.Count; ++i)
+            {
+                // TODO Implement Equals on Wallet
+                Assert.AreEqual(userWallets[i], wallets[i]);
+            }
         }
     }
 }
