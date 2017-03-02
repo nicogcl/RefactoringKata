803ff5a4ea01bcd75c2ce5f2cdf56c42d077f0a0
nicogcl <nicogcl@gmail.com>
2017-03-02 21:58:54 +0100

    Improved exception implementations
    Follow microsoft recommendations

--- WalletKata/Exceptions/ThisIsAStubException.cs
+++ WalletKata/Exceptions/ThisIsAStubException.cs
@@ -4,7 +4,17 @@ namespace WalletKata.Exceptions
 {
     public class ThisIsAStubException : Exception
     {
+        // Recommended Microsoft implementation have 3 constructors
+
+        public ThisIsAStubException() 
+        {
+        }
+
         public ThisIsAStubException(string message) : base (message)
+        {
+        }
+
+        public ThisIsAStubException(string message, Exception inner) : base(message, inner)
         {
         }
     }
--- WalletKata/Exceptions/UserNotLoggedInException.cs
+++ WalletKata/Exceptions/UserNotLoggedInException.cs
@@ -4,5 +4,18 @@ namespace WalletKata.Exceptions
 {
     public class UserNotLoggedInException : Exception
     {
+        // Recommended Microsoft implementation have 3 constructors
+
+        public UserNotLoggedInException()
+        {
+        }
+
+        public UserNotLoggedInException(string message) : base (message)
+        {
+        }
+
+        public UserNotLoggedInException(string message, Exception inner) : base(message, inner)
+        {
+        }
     }
 }
